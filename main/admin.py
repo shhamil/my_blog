10d5bb9bf49ae90faf7c45e465b8667800f476a5
Shamil <kurban0595@inbox.ru>
2020-10-12 19:51:25 +0300

    Added the model to the admin panel

--- main/admin.py
+++ main/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import AdvUser, Post, Tag
+from .models import AdvUser, Post, Tag, Comment
 
 admin.site.register(AdvUser)
 admin.site.register(Post)
 admin.site.register(Tag)
+admin.site.register(Comment)
 # Register your models here.
